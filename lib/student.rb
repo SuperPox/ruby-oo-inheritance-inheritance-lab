b44f54217364cce101efb51a63bac9115c0b7d01
SuperPox <calebkohl@gmail.com>
2021-01-27 12:48:54 -0700

    commit tree 84b09b6e829e7a900e442e4adbe0753e29da28a0

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative "./user.rb"
+require 'pry'
+class Student < User
+    attr_accessor :knowledge
+
+    
+
+    def initialize 
+        @knowledge = []
+    end
+
+    def learn(data)
+        @knowledge << data
+    end
+        
+    def knowledge
+        return @knowledge
+    end
+
 
 end
